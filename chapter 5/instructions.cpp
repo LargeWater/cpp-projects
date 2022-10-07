56176c62cf86221a92044d4de7c74aa84f8fa12e
Nicholas Bailey <nickcyg1992@gmail.com>
2022-10-07 14:01:40 -0500

    chapter 5 pt 1

---
+++ chapter 5/instructions.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+void instructions();
+
+int main()
+{
+  instructions();
+  return 0;
+}
+
+void instructions()
+{
+  cout << "Welcome to the most fun you have ever had with
+  text!\n\n";
+  cout << "Here is how to play the game...\n";
+}
+
+//vscode is giving me linting for this code and not compiling, despite it being perfectly fine. who knows
